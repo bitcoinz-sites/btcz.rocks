5d54b732736f94b1c15b5b2dca5fa2fe3a0be8d9
Beshoy Girgis <shoy@1ds.us>
2017-12-19 21:03:37 -0600

    Add stocks.exchange
    Changed the layout from 3 exchanges to two exchanges on each row so it
    fits properly.

--- exchange.php
+++ exchange.php
@@ -1,9 +1,15 @@
-<div class="col-md-3 col-sm-4">
-<a href="https://trade.cryptobroker.io/markets/btczbtc" target="_blank"><img src="/img/1.png" class="exch_img"></img></a>
+    <div class="col-md-5">
+        <a href="https://stocks.exchange/trade/BTCZ/BTC" target="_blank"><img src="/img/exchange-stocks-exchange.png" class="exch_img"></img></a>
+    </div>
+    <div class="col-md-4">
+        <a href="https://tradesatoshi.com/Exchange?market=BTCZ_BTC" target="_blank"><img src="/img/2.png" class="exch_img"></img></a>
+    </div>
 </div>
-<div class="col-md-3 col-sm-4">
-<a href="https://tradesatoshi.com/Exchange?market=BTCZ_BTC" target="_blank"><img src="/img/2.png" class="exch_img"></img></a>
-</div>
-<div class="col-md-3 col-sm-4">
-<a href="https://c-cex.com/?p=btcz-btc" target="_blank"><img src="/img/4.png" class="exch_img"></img></a>
-</div>
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-5">
+        <a href="https://c-cex.com/?p=btcz-btc" target="_blank"><img src="/img/4.png" class="exch_img"></img></a>
+    </div>
+    <div class="col-md-4">
+        <a href="https://trade.cryptobroker.io/markets/btczbtc" target="_blank"><img src="/img/1.png" class="exch_img"></img></a>
+    </div>
