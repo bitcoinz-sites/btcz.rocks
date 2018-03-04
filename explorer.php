60b7496c59afd8dbd6d967e53a24a3fd907ccbac
Beshoy Girgis <shoy@1ds.us>
2018-03-04 01:15:29 -0600

    Delete all the things

--- explorer.php
+++
@@ -1,3 +0,0 @@
-<?php /* <li><a href="https://explorer.bitcoinz.site/" target="_blank">Explorer.bitcoinz.site</a></li> */ ?>
-<li><a href="https://bitcoinz.ph/" target="_blank">Bitcoinz.ph</a></li>
-<li><a href="https://btczexplorer.blockhub.info/" target="_blank">Btczexplorer.blockhub.info</a></li>
--- socialmobile.php
+++
@@ -1,7 +0,0 @@
-<li><a href="https://www.facebook.com/BTCZCommunity/" target="_blank">Facebook</a></li><br>
-<li><a href="https://slack.bitcoinz.global/" target="_blank">Slack</a></li><br>
-<li><a href="https://twitter.com/BitcoinzTeam" target="_blank">Twitter</a></li><br>
-<li><a href="https://t.me/joinchat/CDzlaRGMvBm4P2Z76sNclQ" target="_blank">Telegram</a></li><br>
-<li><a href="https://discordapp.com/invite/u3dkbFs" target="_blank">Discord</a></li><br>
-<li><a href="https://www.reddit.com/r/BTCZCommunity/" target="_blank">Reddit</a></li>
-<li><a href="https://bitcointalk.org/index.php?topic=2166510.0" target="_blank">VIEW FORUM bitcointalk</a></li>
