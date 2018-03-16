fa59b8563c50d8996dab0ef29d0fca80ac52d3aa
Beshoy Girgis <shoy@1ds.us>
2018-03-16 10:48:27 -0500

    Add Romanian translation
    @awreeoh on Discord was kind enough to provide us with Romanian
    translations!

--- translation/meta_languages.php
+++ translation/meta_languages.php
@@ -27,6 +27,12 @@ return [
             'short' => 'nl',
             'icon' => 'nl'
         ],
+        'ro' => [
+            'native' => 'Romana',
+            'in_english' => 'Romanian',
+            'short' => 'ro',
+            'icon' => 'ro'
+        ],
         'sr' => [
             'native' => 'Srpski',
             'in_english' => 'Serbian',
