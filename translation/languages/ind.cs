4994996e3aad21ac2fb01c9b6c8a968462aea183
wowprc <korcae@gmail.com>
2018-05-10 07:06:57 +0200

    Create ind.cs

---
+++ translation/languages/ind.cs
@@ -0,0 +1,118 @@
+Your Financial Freedom | BitcoinZ	Kebebasan Finansial Anda | BitcoinZ
+BitcoinZ is the future of cryptocurrency. Welcome to decentralization.	Selamat Datang di Desentralisasi. BitcoinZ. Hadiah kepada dunia dari komunitas
+COMMUNITY_PAPER_URL	https://drive.google.com/file/d/19eJ09gwba5Ix9k3Y8Kk7NqdTbnUTFXDj
+Home	rumah
+Community Paper	CATATAN KOMUNITAS
+Benefits	MANFAAT
+Exchange	PERTUKARAN
+Wallets	DOMPET
+Guides	PERTAMBANGAN
+Pools	PERTAMBANGAN
+Explorer	JELAJAH
+Social	SOSIAL
+Lang	Lang
+It's Your Coin	Itu koinmu
+BitcoinZ: The true Bitcoin 2.0	BitcoinZ: The true Bitcoin 2.0
+Welcome to the decentralization	Selamat Datang di Desentralisasi.
+Start Mining	Start Mining
+Latest News	Latest News
+Donate	Donate
+About the <span>coin</span>	TENTANG BTCZ
+Important Details	INFORMASI PENTING
+- Backup your wallet in many locations<br>- Store big amount of coins in cold wallets in anonymous addresses<br>- Don't give / send anyone your private keys. Private keys needs to be secured. You can recover your wallets. If you give or send anyone your private key please consider it as a red flag and quickly move your coins to other address	"INFORMASI PENTING
+- Cadangkan dompetmu di banyak lokasi
+- Simpan koin dalam jumlah besar di dompet dingin dengan alamat anonim
+- Jangan memberi/mengirimkan kunci khusus kepada siapapun. Kunci khusus adalah satu – satunya kunci
+yang kamu gunakan untuk membuka alamat koin. Jika kamu memberi/mengirimkannya kepada
+seseorang, pastikan kamu mewanti – wanti dengan sangat waspada dan segera pindahkan seluruh koinmu
+ke alamat yang berbeda."
+BitcoinZ is <span>Bitcoin 2.0</span>	BitcoinZ ADALAH<span>Bitcoin VERSI 2.0</span>
+- Bitcoin technology<br>- Anonymous transactions (zk-SNARKs privacy)<br>- Bitcoin has all transactions publicly available.<br>- decentralized GPU mining (ASIC miners are banned, hardforks to prevent ASIC miners are allowed)<br>- Decentralized development / All contributors are volunteers<br>- Big blocks -> blocks are similar to BCC / BCH	"- Teknologi Bitcoin
+- Transaksi anonim (privasi zk-SNARKs) – Semua transaksi bitcoin dapat dilihat oleh semua orang
+(umum)
+- Pertambangan desentralisasi menggunakan GPU (dilarang menambang menggunakan ASIC,
+hardfork untuk mencegak penambang ASIC diijinkan)
+- Pengembangan desentralisasi/semua kontribusi berasal dari relawan
+- Blok lebih besar  Blok mirip dengan BCC/BCH"
+Active <span>Community</span>	Active <span>Community</span>
+Our community is active and we are always developing. <br>You can follow us in social networks, on the forum, as well as Slack	"Kami adalah komunitas aktif yang terus mengembangkan bitcoinz.
+Kamu dapat mengikuti kami di berbagai jejaring social, forum dan tentu saja di Slack"
+Benefits <span>BitcoinZ</span>	Benefits <span>BitcoinZ</span>
+Max Supply	Max Supply
+21,000,000,000 BTCZ	21,000,000,000 BTCZ
+Algorithm	Algorithm
+Equihash	Equihash
+Fast Transactions	Fast Transactions
+2MB every 2.5 minutes	2MB every 2.5 minutes
+Circulating Supply	Circulating Supply
+12,500 coins every 2.5 minutes	12,500 coins every 2.5 minutes
+Our goal is to port all best free software on the market to be able to use in BitcoinZ.	"Tujuan kami adalah untuk menjembatani semua perangkat lunak terbaik dapat dengan mudah menggunakan
+BitcoinZ."
+<p>Immutable params:</p> <p>- max supply </p> <p>- inflation </p> <p>- only POW algo - we may change Equihash algo to other POW algo to prevent ASIC miners </p> <p>- new features are allowed to improve usage / scalability, but we will never change history (ETH/ETC case) </p>	"Catatan Khusus :
+- Total Suplai
+- Inflasi
+- Hanya menggunakan algoritma POW – Kami mengubah algoritma Equihash dengan algoritma POW
+untuk membatasi penambang menggunakan komputer ASIC.
+- Mengijinkan Fitur baru untuk meningkatkan skala pemakaian, tapi kami tidak akan mengubah catatan
+sejarah (seperti ETH/ETC)"
+<ul class="list"> <li>Algo: Equihash (always GPU mining - we promote decentralization in mining) </li> <li>Max supply 21B coins </li> <li>Current supply: 12,500 coins every 2.5 minutes</li> <li>Current block size is similar to BCC/BCH (BTCZ = 2MB every 2.5 mins ~ BCC/BCH = 8MB every 10 min)</li> </ul>	"Algoritma : Equihash (Hanya menambang menggunakan GPU – kami mempromosikan penambangan
+desentralisasi)
+Total koin : 21 milyar koin
+Suplai saat ini : 12500 koin setiap 2.5 menit
+Ukuran blok saat ini mirip dengan BCC/BCH (BTCZ = 2MB setiap 2.5 menit ~ BCC/BCH = 8MB setiap 10
+menit)"
+View Forum	View Forum
+Why <span>BitcoinZ?</span>	KENAPA<span>BitcoinZ?</span>
+We are a fully decentralized network	KAMI SEPENUHNYA JARINGAN DESENTRALISASI
+Here are some advantages	Inilah beberapa kelebihannya
+Anonymity	ANONIM
+All transactions are completely anonymous.	Semua transaksi benar – benar anonim
+Support for different platforms	MENDUKUNG PLATFORM YANG BERBEDA
+Our wallet is compatible with Linux, Windows, iOS and Android.	"Dompet kami dapat berjalan dengan baik di mac, linux, windows, ios dan android. Kami akan terus
+berbenah diri."
+Community	KOMUNITAS
+We listen to our dear community, so we can succeed.	Kami mendengarkan komunitas sehingga kami akan berhasil
+Active price growth	HARGA TERUS BERKEMBANG
+BitcoinZ is focused on organic growth.	BitcoinZ focus pada pertumbuhan organik
+Exchanges	Exchanges
+Buy or sell your BitcoinZ coins	Beli atau jual BitcoinZ mu
+Download <span>Wallet</span>	Download <span>Wallet</span>
+Linux, Windows and mobile wallets	Linux, Windows and mobile wallets
+Road <span>Map</span>	Road <span>Map</span>
+BitcoinZ Core Client	BitcoinZ Core Client
+Block Explorer	Block Explorer
+Mining Pools	Mining Pools
+GUI Wallet	GUI Wallet
+iOS &amp; Android Wallet	iOS &amp; Android Wallet
+Commerce Plugins	Commerce Plugins
+Exchange Listing	Exchange Listing
+Masquerade Technology	Masquerade Technology
+Decentralization Packages	Decentralization Packages
+Game Development	Game Development
+zk-SNARKs From Mobile	zk-SNARKs From Mobile
+Public Relations	Public Relations
+why choose <span>BitcoinZ</span>	mengapa memilih <span>BitcoinZ</span>
+Here are a few <span>reasons</span>	Berikut adalah beberapa <span> alasan </span>
+WARNING: Always remember about the pitfalls and keep your backup keys in a safe place!	WARNING: Always remember about the pitfalls and keep your backup keys in a safe place!
+Decentralized <span>Mining</span>	PERTAMBANGAN DESENTRALISASI
+As a miner you should not use the biggest pools to follow main principles.<br>Easy to mine<br>Equihash algorithm.<br>Decentralized Exchanges	"Sebagai penambang sebaiknya kamu tidak menggunakan pertambangan terbesar untuk mengikuti prinsip
+utama.Mudah untuk menambang Algoritma Equihash"
+No <span>Pre-Mine</span>	TIDAK ADA PRA-TAMBANG
+Pure coin. No pre-mine.	Koin Murni. Tanpa Pra-Tambang
+21 billion coins! Everyone in the world can have at least one BTCZ!	- 21 milyar koin – Semua orang di seluruh dunia dapat memiliki setidaknya satu BTCZ
+Our community is actively developing and growing every day.	KOMUNITAS AKTIF Komunitas kita aktif dan berkembang setiap hari
+Anonymous <span>Transactions</span>	TRANSAKSI ANONIM
+All transactions remain anonymous. So you do not have to worry about anonymity.	TRANSAKSI ANONIM Gunakan BitcoinZ di Smartphonemu
+Android and iOS <span>Wallet</span>	Android and iOS <span>Wallet</span>
+You can use BitcoinZ coins in your smartphones.	Gunakan BitcoinZ di Smartphonemu
+Natural <span>Growth</span>	BERKEMBANG ALAMI
+Your coin will grow and develop in a natural way.	Koinmu akan tumbuh dan berkembang secara alami
+Decentralized <span>Development</span>	PENGEMBANGAN DESENTRALISASI
+All exchanges are allowed. The best ones are decentralized. We plan to implement fully decentralized XCAT exchange.	"Semua pertukaran diijinkan. Yang terbaik adalah desentralisasi. Kami berencana untuk sepenuhnya
+desentralisasi"
+Only POW and <span>no POS</span>	HANYA POW DAN TIDAK POS
+The coin is calculated only for POW mining, and will never be changed to POS	Koin dikalkulasi hanya menggunakan pertambangan POW dan tidak akan pernah berubah menjadi POS
+View Bitcointalk Forum Topic	View Bitcointalk Forum Topic
+Bitcointalk Forum Topic	Bitcointalk Forum Topic
+BitcoinZ Community. All rights reserved.	Komunitas. Seluruh hak cipta
+	
